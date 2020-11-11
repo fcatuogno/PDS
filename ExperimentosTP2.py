6a4a59d65c9e7d4f32c9b83305b05c834fbd3930
Fabian <fabian.2mpr@hotmail.com>
2020-11-10 23:45:41 -0300

    Se resuelve de manera matricial

--- ExperimentosTP2.py
+++ ExperimentosTP2.py
@@ -23,70 +23,86 @@ fs = 90 # Hz
 # a.4) Senoidal #
 #################
 
+#Tono 1
 a1 = 1     # Volts
 p1 = 0     # radianes
 f1 = fs/4 # Hz
-
-a2_dB = -40
-
-a2 = 10**(a2_dB/20)
-f2 = f1 + 10*fs/N
 
 Offset = 0 #Volt
 p0 = 0
 
 
-tus_resultados = []
+#Tono 2
+a2_dB = np.linspace(-300,-100,10)
+# a2_dB = np.array([-40])
+a2 = 10**(a2_dB/20)
+f2 = f1 + 10*fs/N
+
+######################################
+#Generacion de matriz tiempo
+######################################
+t0 = np.zeros(len(a2))
+tf = np.ones(len(a2))*(N-1)/fs
+
+tt = np.linspace(t0, tf, N)
+# tt = np.arange(0.0, N/fs, 1/fs)
+
+######################################
+#GEneracion de tonos
+######################################
+
+matriz = a2*np.sin((2*np.pi*f2*tt+p1))
+matriz += a1*np.sin((2*np.pi*f1*tt+p1))
+
+#######################################
+#Generacion de Ventanas
+#######################################
+
+Ventanas = { 0:'Rectangular', 1:'Bartlett', 2:'Hanning', 3:'Blackman', 4:'Flattop' }
+Ventanas = { 0:'Rectangular'}
+
+# Generacion de Matriz con Ventanas
+########################################################
+VentRect = np.ones(N).reshape((N,1))
+VentBarlett = signal.windows.bartlett(N).reshape((N,1))
+VentHann = signal.windows.hann(N).reshape((N,1))
+VentBlack = signal.windows.blackman(N).reshape((N,1))
+VentFlat = signal.windows.flattop(N).reshape((N,1))
+
+ventana = np.column_stack(([VentRect, VentBarlett,VentHann, VentBlack, VentFlat]))
+
+##############################################
+#Graficas temporales
+############################################
 plt.close('all')
-fig, [ax1, ax2] = plt.subplots(1,2)
 
-                   
-fd = [0.5]
+# fig, ax1 = plt.subplots()
 
-VentBarlett = signal.windows.bartlett(N)
-VentHann = signal.windows.hann(N)
-VentBlack = signal.windows.blackman(N)
-VentFlat = signal.windows.flattop(N)
-
-
-for f0 in fd:
-    f1 = fs/4 + f0*fs/N
-    f2 = f1 + 10*fs/N
-        
-    tiempo, xx = my.mi_funcion_sen( a1, Offset, f1, p0, N, fs)
-    tiempo2, xx2 = my.mi_funcion_sen( a2, Offset, f2, p0, N, fs)
-    xx += xx2
-    xx_Barlett = xx*VentBarlett
+# for index in np.arange(len(a2)):
     
-    espectro = np.abs(np.fft.fft(xx))/len(xx)
-    espectro = np.fft.fftshift(espectro)
+ #    ax1.plot(tt[0:,index],matriz[0:,index])
+
+############################################
+#Calculo de DFT
+###########################################
+for index in Ventanas:
+    
+    espectro = np.abs(np.fft.fft(matriz*ventana[0:,index].reshape(N,1), axis=0))*N
+    espectro = np.fft.fftshift(espectro, axes=0)
     espectro = 20*np.log10(espectro)
-    
-    espectro_Barlett = np.abs(np.fft.fft(xx_Barlett))/len(xx)
-    espectro_Barlett = np.fft.fftshift(espectro_Barlett)
-    espectro_Barlett = 20*np.log10(espectro_Barlett)                                
-    
-    frec = np.fft.fftfreq(len(xx),1/N) #Espectro en bines
-    frec = np.fft.fftshift(frec)
 
-      
-    ax1.plot(tiempo, xx, label='Señal sin Ventana')
-    ax1.plot(tiempo, xx_Barlett, label='Señal con Ventana Barlett')
+    frec = np.fft.fftfreq(espectro.shape[0],0.5) #Normalizo respecto a Fs
+    frec = np.fft.fftshift(frec)    
 
-    ax2.plot(frec,espectro, label='Espectro Señal Original')
-    ax2.plot(frec,espectro_Barlett, label='Espectro con Ventana Barlett')
-    
-    ax1.set_title('Forma de onda temporal señal bitonal', fontsize = 'xx-large')
-    ax1.set_ylabel('Amplitud')
-    ax1.set_xlabel('Tiempo [s]')
-    
-    ax2.set_title('Espectro Señal bitonal', fontsize = 'xx-large')
-    ax2.set_ylabel('Modulo [dB]')
-    ax2.set_xlabel('Frec [bin]')
-    ax2.grid()
-    plt.xlim(0, N/2)
-    
-    
-    ax1.legend(loc='upper right', fontsize='x-large')  # Add a legend.
-    ax2.legend(loc='lower right', fontsize='x-large')  # Add a legend.
+    fig, ax1 = plt.subplots()
+        
+    for nn,aa in zip(np.arange(len(a2)), a2_dB):
+    # for nn in np.arange(len(a2)):
+
+        ax1.plot(frec,espectro[0:,nn], label='%i dB' %aa)
+        ax1.set_title('Espectro Ventana %s' %Ventanas[index], fontsize = 'xx-large')
+        plt.xlim(0.4,0.6)    
+        ax1.set_ylabel('Amplitud')
+        ax1.set_xlabel('N')
+        ax1.legend(loc='upper right', fontsize='x-large')  # Add a legend.
 
